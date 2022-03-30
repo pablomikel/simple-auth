3103e08b92cc3425e64d539be2d117ab40aac7e4
Pablo Olavarrieta <pablo.olavarr@gmail.com>
2022-03-30 11:34:02 -0700

    more changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,10 @@ const AboutPage = () => {
     <Layout>
       <Status />
       <View title="About">
+        <p>
+          This is a statically generated page that is always publicly visible
+          and leverages Gatsby tools like 'gatsby-plugin-image'.
+        </p>
         <StaticImage src="https://images.unsplash.com/photo-1647955043361-ea00032b8d87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
       </View>
     </Layout>
